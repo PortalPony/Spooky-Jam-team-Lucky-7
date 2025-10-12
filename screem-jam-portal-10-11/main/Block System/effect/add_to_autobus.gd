7249cb1ee1a72c230eac4dd6fbaa0f2b34f5557c
Chrisp <138921357+PortalPony@users.noreply.github.com>
2025-10-12 09:10:49 -0700

    6th IDK what this is

--- screem-jam-portal-10-11/main/Block System/effect/add_to_autobus.gd
+++ screem-jam-portal-10-11/main/Block System/effect/add_to_autobus.gd
@@ -3,7 +3,11 @@ extends Node2D
 
 @export var thing := "breakerBox"
 
+@export var oneShot = false
+
 func action(amount: int):
 	# Get the current value of Inventory[thing] and add amount
 	Inventory.set(thing, Inventory.get(thing) + amount)
 	print_debug(Inventory.get(thing))
+	if oneShot:
+		queue_free()
