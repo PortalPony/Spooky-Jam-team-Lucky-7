348d1c9a56858c9627bc9e828f7dfbad60ca4f2f
spanner234 <briggspencer34@gmail.com>
2025-10-12 15:42:14 +0100

    enemy

--- screem-jam-portal-10-11/main/scripts/weeping_angel.gd
+++ screem-jam-portal-10-11/main/scripts/weeping_angel.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
+@export var target = Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	if target:
+		look_at(target.global_position)
+		position += Vector2.RIGHT.rotated(rotation) * 100 * delta
+		move_and_slide()
+		pass
